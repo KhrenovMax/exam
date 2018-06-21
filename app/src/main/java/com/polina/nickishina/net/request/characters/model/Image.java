62477c9fdfb5c9f5b9a08b217d4f043f9ab8ff9e
nickishina <nickishina.polina002@gmail.com>
2018-06-21 14:48:34 +0300

    for my lovely

---
+++ app/src/main/java/com/polina/nickishina/net/request/characters/model/Image.java
@@ -0,0 +1,36 @@
+package com.polina.nickishina.net.request.characters.model;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.io.Serializable;
+
+public class Image implements Serializable{
+    @SerializedName("path")
+    private String path;
+    @SerializedName("extension")
+    private String extension;
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getExtension() {
+        return extension;
+    }
+
+    public void setExtension(String extension) {
+        this.extension = extension;
+    }
+
+    @Override
+    public String toString() {
+        return "Image{" +
+                "path='" + path + '\'' +
+                ", extension='" + extension + '\'' +
+                '}';
+    }
+}
